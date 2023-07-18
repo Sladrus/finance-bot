ea302ae41c0e8d25a4152d8d88ce7156e0b2e272
Sladrus <ufimkinyasha@gmail.com>
2023-07-18 09:59:09 +0300

    fix

--- commands/i.js
+++ commands/i.js
@@ -4,7 +4,7 @@ module.exports = async function getChatIdSleepGroup(bot, msg, args) {
   if (msg.chat.type === 'private') return;
   if (msg.chat.id !== -1001935148888) return;
   try {
-    const link = await bot.exportChatInviteLink(-1001897613970);
+    const link = await bot.exportChatInviteLink(-1001902015957);
     await bot.sendMessage(-1001935148888, link);
     console.log(link);
   } catch (e) {
